36a10b1a7cd23340d9719fd46dee25432d1a07b2
liuyufan130722 <132428694+liuyufan130722@users.noreply.github.com>
2023-12-15 09:12:00 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from random import *
+def outtype(str):
+    print(str)
+def intype(str):
+    input(str) 
+def RI(upRadius,downRadius):
+    result = randint(upRadius,downRadius)
+    return result
